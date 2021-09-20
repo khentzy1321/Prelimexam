8262efcdf2a360071ab74c9c4adf278b822d29ea
Kent Jeremai Abarquez <k2khenzkiemeliodas@gmail.com>
2021-09-20 04:37:47 +0100

    First commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Demo</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+</head>
+<body>
+    <h1>Laravel Demo</h1>
+    <a href="{{url('/')}}">Home</a> | Profile
+</body>
+</html>
---
+++ resources/views/pages/profile.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Demo | Profile</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+</head>
+<body>
+<h1>Profile Page</h1>
+    
+</body>
+</html>
